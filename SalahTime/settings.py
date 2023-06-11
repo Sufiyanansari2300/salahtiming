8259ecdab3d37020e6b6cc8a80c41e35fb7ced13
Sufiyan <sufiyanansari2300@gmail.com>
2023-06-12 01:31:28 +0530

    salah timing

--- SalahTime/settings.py
+++ SalahTime/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(-phn+z!k%2uddn^w%5yhd)nfpc1x8%mcc(#m1=m^d0=(^b^5*'
-x
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
